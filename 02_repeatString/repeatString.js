f737638b88650c94403cc0283006255f43586b55
Frank Huang <frankluxinghuang@gmail.com>
2022-12-12 21:11:41 -0500

    Completed first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+var stringContainer = '';
+if (x < 0) {
+    stringContainer += 'ERROR';
+} else {
+for (i=0; i<x; i++) {
+        stringContainer += string;   
+}
+}
+return stringContainer;
 };
-
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
