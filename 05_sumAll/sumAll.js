85c1df5fef9e740fce03efaade021b52a57b076a
Frank Huang <frankluxinghuang@gmail.com>
2022-12-12 22:37:58 -0500

    Completed exercise five all by myself

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+var theSum = 0;
+if (x<0 || y<0) {
+    theSum = 'ERROR';
+} else if (!Number.isInteger(x) || !Number.isInteger(y)){
+    theSum ='ERROR';
+} else if (x > y) {
+    for (i=y; i<(x+1); i++) {
+        theSum += i;
+    }
+} else if (x < y) {
+    for (i=x; i<(y+1); i++) {
+        theSum += i;
+    }
+} else {
+    theSum = x;
+}
+return theSum;
 };
 
 // Do not edit below this line
