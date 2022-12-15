902074ea7a9779f4b618b720b4ba6c8a9a58a811
Frank Huang <frankluxinghuang@gmail.com>
2022-12-14 22:48:30 -0500

    Completed exercise ten kind of a doozy but also kinda easy

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+let first = 1;
+let second = 1;
+if (x<0){
+    return "OOPS";
+} else {
+for(i=2;i<x;i++){
+var fib = first + second;
+first = second;
+second = fib;
+}
+return second;
+}
 };
 
 // Do not edit below this line
