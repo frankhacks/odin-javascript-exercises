6f90e4da57ea5b6efc51b1dcd74b9507ad3aed86
Frank Huang <frankluxinghuang@gmail.com>
2022-12-14 22:35:42 -0500

    Completed lesson nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+let lower = string.toLowerCase();
+let periods = lower.split(".").join("");
+let commas = periods.split(",").join("");
+let exclaim = commas.split("!").join("");
+let stringy = exclaim.split(" ").join("");
+if (stringy === stringy.split("").reverse().join("")) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
