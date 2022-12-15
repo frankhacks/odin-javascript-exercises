811ce17d2a1bcc48870211dc88a9f6c7f75e0bba
Frank Huang <frankluxinghuang@gmail.com>
2022-12-14 22:22:02 -0500

    Completed exercise eight fairly easily

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+let	addition = x + y;
+  return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+let	subtraction = x - y;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sums = 0;
+	for (items of array) {
+    sums+=items;
+  }
+  return sums;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multplication = 1;
+for (items of array){
+ multplication*=items;
+}
+return multplication;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let fact = 1;
+  for (i = x; i>0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
